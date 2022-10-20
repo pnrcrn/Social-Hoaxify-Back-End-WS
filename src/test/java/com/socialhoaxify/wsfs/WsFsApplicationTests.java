ba4f27922f8175ef5cd0aa3fc31277dc5238557a
Pınar Ceran <pinarceran98@gmail.com>
2022-10-20 23:04:01 +0300

    Create WsFsApplicationTests.java

---
+++ src/test/java/com/socialhoaxify/wsfs/WsFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialhoaxify.wsfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
